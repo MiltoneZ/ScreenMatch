2e6d6f6cbc15a51db6038ee7338c1b73b5527c76
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-17 15:15:09 -0600

    Uso de nuevos objetos.

--- src/Principal.java
+++ src/Principal.java
@@ -2,6 +2,17 @@ public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
 
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionEnMinutos = 120;
+        System.out.println("Mi pelicula es: " + miPelicula.nombre);
+        System.out.println("La fecha de lanzamiento fue: " + miPelicula.fechaDeLanzamiento);
 
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionEnMinutos = 180;
+        System.out.println("Mi pelicula es: " + otraPelicula.nombre);
+        System.out.println("La fecha de lanzamiento fue: " + otraPelicula.fechaDeLanzamiento);
     }
 }
