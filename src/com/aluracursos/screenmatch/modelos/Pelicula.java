3734c150000461466a8d43118507ba4cffc7df61
MiltoneZ <milton.uzieldm@gmail.com>
2024-04-23 19:17:43 -0600

    Refactorizando las clases.

--- src/com/aluracursos/screenmatch/modelos/Pelicula.java
+++ src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -1,41 +1,13 @@
 package com.aluracursos.screenmatch.modelos;
 
-public class Pelicula {
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnElPlan;
-    private double sumaDeLasEvaluaciones;
-    private int totalDeLasEvaluaciones;
+public class Pelicula extends Titulo{
+    private String director;
 
-    public void setNombre(String nombre){
-        this.nombre = nombre;
+    public String getDirector() {
+        return director;
     }
 
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-        this.incluidoEnElPlan = incluidoEnElPlan;
-    }
-
-    public void muestraFichaTecnica(){
-        System.out.println("El nombre de la pelicula es: " + nombre);
-        System.out.println("La fecha de lanzamiento fue: " + fechaDeLanzamiento);
-        System.out.println("Durancion en minutos: " + duracionEnMinutos);
-    }
-
-    public void evalua(double nota){
-        sumaDeLasEvaluaciones += nota;
-        totalDeLasEvaluaciones ++;
-    }
-
-    public double calularMedia(){
-        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
